3e8cf2ff123920cc363c3c4bc901dacab27810bd
davidloz00 <94832155+davidloz00@users.noreply.github.com>
2026-08-16 15:17:26 -0700

    fix GitHub pages base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://davidloz00.github.io',
+  base: '/artist-archive',
   trailingSlash: 'ignore',
   build: {
     format: 'directory'
